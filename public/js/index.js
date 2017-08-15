afecb985f45dd20490d4002c2878277c5ea363c0
vinicius-sabadim <vinicius.sabadim@gmail.com>
2017-08-15 01:08:10 -0300

    Melhorando o código e adicionando funcionalidades

--- public/js/index.js
+++ public/js/index.js
@@ -2,134 +2,148 @@
 
 	angular
 		.module('app')
-		.controller('PrincipalController', PrincipalController);
+		.controller('PrincipalController', PrincipalController)
 
 	PrincipalController.$inject = ['$scope'];
 
 	function PrincipalController($scope) {
 
-		$scope.tratarTecla = tratarTecla;
-		$scope.adicionar = adicionar;
-		$scope.toggleBotao = toggleBotao;
-		$scope.toggleHistorico = toggleHistorico;
-
-		function init() {
-			$scope.textoBotao = 'Ocultar';
-
-			$scope.mostrarTela = false;
-
-			$scope.mostrarHistorico = false;
-			$scope.historico = [];
-
-			$scope.eventos = [new Evento('Tempo', 'Q', 'W'), new Evento('Contador', 'A')];
+		const Evento = {
+			tempo: false,
+			contador: 0,
+			inicio: 0,
+			duracao: 0,
+			tecla: '',
+			teclaFim: ''
 		}
 
-		function Evento(nome, tecla, tecla2) {
-			nome = nome || '';
-			tecla = tecla || '';
-			tecla2 = tecla2 || '';
-
-			this.nome = nome;
-			this.tempo = !!tecla2;
-			this.config = {
-				tecla: tecla,
-				teclaFim: tecla2,
-				inicio: 0,
-				duracao: 0,
-				contador: 0
-			};
-			return this;
-		}
-
-		function Historico(nome, tipo, posicao, iniciou) {
-			iniciou = iniciou || null;
-
-			this.nome = nome;
-			this.tipo = tipo;
-			this.posicao = formatarHorario(posicao);
-			this.iniciou = iniciou;
-			if (tipo === 'tempo') {
-				this.icone = iniciou ? 'fa-play' : 'fa-stop';
-			} else {
-				this.icone = 'fa-plus'
-			}
-			return this;
-		}
-
-		function formatarHorario(posicao) {
+		const formatarHorario = (posicao) => {
 
 			if (posicao === 0) {
-				return '0:00';
+				return '0:00'
 			}
 
-			posicao = (posicao / 60).toFixed(3);
-			posicao = String(posicao);
-			tempo = (posicao.split('.'));
-			minutos = tempo[0];
-			segundos = tempo[1];
-			segundos = parseInt(segundos);
-			segundos = (segundos * 60) / 1000;
-			segundos = Math.round(segundos);
+			posicao = (posicao / 60).toFixed(3)
+			posicao = String(posicao)
+			tempo = (posicao.split('.'))
+			minutos = tempo[0]
+			segundos = tempo[1]
+			segundos = parseInt(segundos)
+			segundos = (segundos * 60) / 1000
+			segundos = Math.round(segundos)
 
 			if (segundos < 10) {
-				segundos = '0' + segundos;
+				segundos = '0' + segundos
 			}
 
-			return minutos + ':' + segundos;
+			return minutos + ':' + segundos
 		}
 
-		function toggleBotao() {
-			$scope.mostrarTela = !$scope.mostrarTela;
-			$scope.mostrarTela ? $scope.textoBotao = 'Mostrar' : $scope.textoBotao = 'Ocultar';
+		const toggleBotao = () => {
+			const { mostrarTela } = $scope
+			$scope.mostrarTela = !mostrarTela
+			$scope.textoBotao = !mostrarTela ? 'Mostrar as configurações' : 'Ocultar'
 		}
 
-		function toggleHistorico() {
-			$scope.mostrarHistorico = !$scope.mostrarHistorico;
+		const toggleHistorico = () => {
+			$scope.mostrarHistorico = !$scope.mostrarHistorico
 		}
 
-		function tratarTecla(keyEvent) {
-			quantificar(String.fromCharCode(keyEvent.keyCode));
+		const tratarTecla = (keyEvent) => {
+			const { keyCode } = keyEvent
+			if (keyCode >= 65 && keyCode <= 90) {
+				quantificar(String.fromCharCode(keyCode))
+			}
 		}
 
-		function quantificar(tecla) {
-			var inicio,
-				primeiraTecla,
-				segundaTecla,
-				nome,
-				posicao = document.getElementById('video').currentTime;
+		const quantificar = (tecla) => {
+			let item = getItem(tecla)
 
-			if ($scope.mostrarTela) {
-				for (var i = 0, len = $scope.eventos.length; i < len; i += 1) {
-					nome = $scope.eventos[i].nome;
+			if (item.length === 0) return
+			if (!$scope.mostrarTela) return
 
-					if ($scope.eventos[i].tempo) {
-						inicio = $scope.eventos[i].config.inicio;
-						primeiraTecla = $scope.eventos[i].config.tecla;
-						segundaTecla = $scope.eventos[i].config.teclaFim;
+			item = item[0]
 
-						if ((primeiraTecla === tecla) && (inicio === 0)) {
-							$scope.eventos[i].config.inicio = new Date();
-							$scope.historico.push(new Historico(nome, 'tempo', posicao, true));
-						} else if ((segundaTecla === tecla) && (inicio !== 0)) {
-							$scope.eventos[i].config.duracao += (new Date() - $scope.eventos[i].config.inicio) / 1000;
-							$scope.eventos[i].config.inicio = 0;
-							$scope.historico.push(new Historico(nome, 'tempo', posicao, false));
-						}
-					} else {
-						if ($scope.eventos[i].config.tecla === tecla) {
-							$scope.eventos[i].config.contador += 1;
-							$scope.historico.push(new Historico(nome, 'contador', posicao));
-						}
-					}
+			const { nome, tempo } = item
+			const posicao = document.getElementById('video').currentTime
+
+			if (tempo) {
+				const primeiraTecla = item.tecla.toUpperCase()
+				const segundaTecla = item.teclaFim.toUpperCase()
+
+				if ((primeiraTecla === tecla) && (item.inicio === 0)) {
+					item.inicio = new Date()
+					$scope.historico.push({
+						nome,
+						posicao,
+						icone: 'fa-play'
+					})
+				} else if ((segundaTecla === tecla) && (item.inicio !== 0)) {
+					item.duracao += (new Date() - item.inicio) / 1000
+					item.inicio = 0
+					$scope.historico.push({
+						nome,
+						posicao,
+						icone: 'fa-stop'
+					})
 				}
+			} else {
+				item.contador += 1
+				$scope.historico.push({
+					nome,
+					icone: 'fa-plus'
+				})
 			}
 		}
 
-		function adicionar() {
-			$scope.eventos.push(new Evento());
+		const getItem = (tecla) => {
+			return $scope.eventos.filter((item) => {
+				return item.tecla.toUpperCase() === tecla || item.teclaFim.toUpperCase() === tecla
+			})
 		}
 
-		init();
+		const adicionar = () => {
+			$scope.eventos.push({
+				...Evento,
+				nome: 'Novo evento'
+			})
+		}
+
+		const salvar = () => {
+			const eventos = JSON.stringify($scope.eventos)
+			window.localStorage.setItem('eventos', eventos)
+		}
+
+		const limpar = () => {
+			window.localStorage.clear()
+		}
+
+		const limparHistorico = () => {
+			$scope.historico = []
+			$scope.eventos.forEach((item) => {
+				item.contador = 0
+				item.duracao = 0
+			})
+		}
+
+		$scope.tratarTecla = tratarTecla
+		$scope.adicionar = adicionar
+		$scope.salvar = salvar
+		$scope.limpar = limpar
+		$scope.limparHistorico = limparHistorico
+		$scope.toggleBotao = toggleBotao
+		$scope.toggleHistorico = toggleHistorico
+
+		const init = () => {
+			$scope.textoBotao = 'Ocultar'
+			$scope.mostrarTela = false
+			$scope.mostrarHistorico = false
+			$scope.historico = []
+			const eventos = JSON.parse(window.localStorage.getItem('eventos'))
+			$scope.eventos = eventos || []
+		}
+
+		init()
 	}
 
 }());
